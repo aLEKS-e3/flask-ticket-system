4b7c29f2b8f6a2c60eff67336ec40c198f3f3bcc
aLEKS-e3 <chebukin404@gmail.com>
2024-05-27 13:24:51 +0300

    Create forms; Install Flask-WTF

---
+++ app/forms.py
@@ -0,0 +1,53 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField,
+    SelectField,
+    TextAreaField,
+)
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField(
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Register")
+
+    @staticmethod
+    def validate_username(username) -> None:
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different username.")
+
+    @staticmethod
+    def validate_email(email) -> None:
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different email address.")
+
+
+class TicketForm(FlaskForm):
+    status = SelectField(
+        "Status",
+        choices=[
+            ("Pending", "Pending"),
+            ("In review", "In review"),
+            ("Closed", "Closed"),
+        ],
+    )
+    note = TextAreaField("Note", validators=[DataRequired()])
+    submit = SubmitField("Submit")
